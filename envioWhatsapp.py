8feba12755400beff35fbf1fda644913a3504e64
MarcoAlarcon <marcoh.alarcon@icloud.com>
2022-10-27 10:22:19 -0300

    Adicionando projeto no github

---
+++ envioWhatsapp.py
@@ -0,0 +1,50 @@
+from multiprocessing import context
+import pandas as pd
+import time
+from playwright.sync_api import sync_playwright
+
+
+#Informar entre aspas duplas o caminho inteiro do arquivo excel e sua extensão.
+excel = pd.read_excel("C:\projetos\AMC\envioWhatsapp\envioWhatsapp.xlsx")
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://web.whatsapp.com")
+
+    #Espera 15 segundos para validação do do QR code e acesso a conta
+    time.sleep(20)
+
+    for i, linhas in enumerate(excel['Telefone']):
+        #Guarda o número de contato do index i da planilha
+        telefone = excel.loc[i,"Telefone"]
+
+        #Armazena a mensagem que foi digitada na celula abaixo do campo mensagem do excel.
+        mensagem = excel.iloc[0,4]
+        link = f'https://web.whatsapp.com/send?phone={telefone}&text={mensagem}'
+
+        caminhoArquivo = excel.iloc[0,5]
+
+        try:
+            page.goto(link)
+            with page.expect_navigation():
+                page.locator("div[role=/'textbox/']")
+
+            page.locator("[data-testid='compose-btn-send']").click()
+
+            if caminhoArquivo:
+                anexarArquivo = page.locator("//*[@id='main']/footer/div[1]/div/span[2]/div/div[1]/div[2]/div/div")
+                anexarArquivo.click()
+                caixaArquivo = page.locator("//*[@id='main']/footer/div[1]/div/span[2]/div/div[1]/div[2]/div/span/div/div/ul/li[1]/button")
+                with page.expect_file_chooser() as fc_info:
+                    caixaArquivo.click()
+                file_chooser = fc_info.value
+                file_chooser.set_files(caminhoArquivo)
+                botaoEnviar = page.locator("//*[@id='app']/div/div/div[2]/div[2]/span/div/span/div/div/div[2]/div/div[2]/div[2]/div/div")
+                botaoEnviar.click()
+
+            time.sleep(5)
+
+        except:
+            continue
